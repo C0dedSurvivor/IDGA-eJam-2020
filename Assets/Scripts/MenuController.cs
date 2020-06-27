25a94023d6c09a29172e66baf1bbf5b02b74be1b
Jacob Brosler <acingmc@gmail.com>
2020-06-27 16:48:29 -0400

    Added title screen

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject creditsPanel;
+
+    public void ToGame()
+    {
+
+    }
+
+    public void ToTitle()
+    {
+        creditsPanel.SetActive(false);
+    }
+
+    public void ToCredits()
+    {
+        creditsPanel.SetActive(true);
+    }
+}
