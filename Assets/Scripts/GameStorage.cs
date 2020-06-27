ad3eed50462254df324a5d7db3e9993e3adafa5d
Jacob Brosler <acingmc@gmail.com>
2020-06-27 11:24:38 -0400

    Made a dialogue tree node able to change a sprite's image

--- Assets/Scripts/GameStorage.cs
+++ Assets/Scripts/GameStorage.cs
@@ -1,13 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 class GameStorage : MonoBehaviour
 {
     public static Dictionary<string, List<DialogueNode>> interactableDialogues = new Dictionary<string, List<DialogueNode>>();
+    public static Dictionary<string, Sprite> sprites = new Dictionary<string, Sprite>();
+
+    //A serialized list of sprites to assist in loading in necessary images for quick-swapping
+    [SerializeField]
+    private List<Sprite> spriteLoader = new List<Sprite>();
+
+    [SerializeField]
+    private SpriteRenderer dialogueBackground;
 
     void Start()
     {
@@ -16,8 +20,14 @@ class GameStorage : MonoBehaviour
             {
                 new DialogueLine("Speaker 1", "Hi, I'm speaking now."),
                 new DialogueLine("Speaker 2", "And now I'm speaking."),
-                new DialogueLine("Todd", "Buy Skyrim.")
+                new DialogueLine("Todd", "Buy Skyrim."),
+                new DialogueSpriteSwitch(dialogueBackground, "check_mark")
             }
             );
+
+        foreach (Sprite sprite in spriteLoader)
+        {
+            sprites.Add(sprite.name, sprite);
+        }
     }
 }
