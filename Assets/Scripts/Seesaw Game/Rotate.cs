652aed09ad3267b88832c7b50149f476d4d3d3e3
Will Kracke <wjk4218@rit.edu>
2020-06-28 13:38:13 -0400

    Hopefully fixing things

--- Assets/Scripts/Seesaw Game/Rotate.cs
+++ Assets/Scripts/Seesaw Game/Rotate.cs
@@ -7,7 +7,7 @@ public class Rotate : MonoBehaviour
     // Variables
     // This should be a value between -10 -> 10
     public float rotation = 0;
-
+    public float targetrot = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +18,17 @@ public class Rotate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Moves the actual rotation closer to the target rotation
+        if (Mathf.Abs(targetrot - rotation) < 0.01)
+        {
+            rotation = targetrot;
+        }
+        else
+        {
+            rotation = Mathf.Lerp(rotation, targetrot, 0.01f);
+        }
+
+        // Updates the actual visuals
         Vector3 rotValue = new Vector3(0, 0, rotation);
         this.gameObject.transform.rotation = Quaternion.Euler(rotValue);
     }
@@ -25,6 +36,6 @@ public class Rotate : MonoBehaviour
     // Method for rotating the object
     public void rotate(float value)
     {
-        rotation = value;
+        targetrot = value;
     }
 }
